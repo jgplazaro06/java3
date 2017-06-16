783c3b1938229eb6000fbe3d20907e1afcc7164f
Bryan Dela Cruz <bdshi13@outlook.com>
2017-06-17 07:06:10 +0800

    initial ui

---
+++ src/main/resources/public/app/app.state.js
@@ -0,0 +1,49 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app', ['ui.router'])
+        .config(StateConfig);
+
+    function StateConfig($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'app/templates/home.html'
+            })
+            .state('login', {
+                url: '/login',
+                templateUrl: 'app/templates/login.html'
+            })
+            .state('register', {
+                url: '/register',
+                templateUrl: 'app/templates/register.html'
+            })
+            .state('error', {
+                url: '/error',
+                templateUrl: 'app/templates/error.html'
+            })
+            .state('cart', {
+                url: '/cart',
+                templateUrl: 'app/templates/cart.html'
+            })
+            .state('checkout', {
+                url: '/checkout',
+                templateUrl: 'app/templates/checkout.html'
+            })
+            .state('account', {
+                url: '/account',
+                templateUrl: 'app/templates/account.html'
+            })
+            .state('account-update', {
+                url: '/account/update',
+                templateUrl: 'app/templates/account-update.html'
+            })
+            .state('branch', {
+                url: '/branch',
+                templateUrl: 'app/templates/branch.html'
+            });
+    }
+})();
